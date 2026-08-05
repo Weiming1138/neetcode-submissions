c921b76ee09d9453e6073925c3bbe93af29476fd
Weiming Zhao <136867528+Weiming1138@users.noreply.github.com>
2026-08-05 16:30:14 -0400

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        fleet = []
+        pairs = sorted(zip(position, speed), reverse=True) #Arrange it in descending order since only forms fleet with car in front
+
+        for pos, spd in pairs:
+            time = (target - pos) / spd #Calculate the time using position and speed
+            
+            if not fleet: #If the fleet is empty, add the new time
+                fleet.append(time)
+            elif fleet and time > fleet[-1]: #If its not empty and the current time is greater than the top of the stack, form a fleet
+                fleet.append(time)
+        
+        return len(fleet)
+            
+            
+
+
