36da207b041b0e9a4000b504245e4a93ff937734
Weiming Zhao <136867528+Weiming1138@users.noreply.github.com>
2026-08-08 17:55:39 -0400

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        #O(log(n + m)) won't work on leetcode
+        low = 0
+        high = len(matrix) - 1
+        
+        while low <= high:
+            middle_index = (low + high) // 2
+
+            if matrix[middle_index][0] <= target <= matrix[middle_index][-1]:
+                for j in matrix[middle_index]:
+                    if j == target:
+                        return True
+                return False
+            if target < matrix[middle_index][0]:
+                high = middle_index - 1
+            if target > matrix[middle_index][-1]:
+                low = middle_index + 1
+
+        return False
+                
+        
+
+
+                
