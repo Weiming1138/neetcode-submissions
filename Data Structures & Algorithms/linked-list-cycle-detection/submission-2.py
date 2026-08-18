753945479aecf18d5b482e3592772a6b1bd5a87b
Weiming Zhao <136867528+Weiming1138@users.noreply.github.com>
2026-08-18 14:24:24 -0400

    Add: linked-list-cycle-detection - submission-2

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-2.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    #Naive O(1) Memory
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        hash = set()
+
+        curr = head
+        while curr:
+            if curr in hash:
+                return True
+
+            hash.add(curr)
+            curr = curr.next
+        return False
