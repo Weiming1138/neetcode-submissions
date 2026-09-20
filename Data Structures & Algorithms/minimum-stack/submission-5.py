81d1c9a433f7260f240b78ab7d014c41d470f5c7
Weiming Zhao <136867528+Weiming1138@users.noreply.github.com>
2026-09-20 18:38:02 -0400

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.py
@@ -0,0 +1,22 @@
+class MinStack:
+
+    def __init__(self):
+        self.minStack = []
+        self.topStack = []
+    def push(self, val: int) -> None:
+        self.topStack.append(val)
+        if len(self.minStack) == 0 or self.topStack[-1] <= self.minStack[-1]:
+            self.minStack.append(val)
+        pass
+    def pop(self) -> None:
+        popped = self.topStack.pop()
+        if popped == self.minStack[-1]:
+            self.minStack.pop()
+
+    def top(self) -> int:
+        return self.topStack[-1]
+        pass
+    def getMin(self) -> int:
+        return self.minStack[-1]
+        pass
+        
